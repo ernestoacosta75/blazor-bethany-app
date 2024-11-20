50cd6d3cd48b3a2496adcffc0e0b8d9535e7b609
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2024-11-20 10:37:07 +0100

    Handling errors in components. Adding Error Boundaries

--- BethanysPieShopHRM/Components/EmployeeCard.razor.cs
+++ BethanysPieShopHRM/Components/EmployeeCard.razor.cs
@@ -11,5 +11,13 @@ namespace BethanysPieShopHRM.Components
         // Events
         [Parameter] 
         public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (string.IsNullOrEmpty(Employee.LastName))
+            {
+                throw new Exception("last name can't be empty");
+            }
+        }
     }
 }
