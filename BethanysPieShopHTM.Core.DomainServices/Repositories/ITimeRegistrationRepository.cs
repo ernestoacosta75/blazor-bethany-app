27299f73b53f8bbc0f08a9b10259b0f5fb6e2f71
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2024-11-27 17:08:27 +0100

    Adding ITimeRegistrationRepository

---
+++ BethanysPieShopHTM.Core.DomainServices/Repositories/ITimeRegistrationRepository.cs
@@ -0,0 +1,9 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHTM.Core.DomainServices.Repositories
+{
+    public interface ITimeRegistrationRepository<TEntity> where TEntity : class
+    {
+        Task<List<TimeRegistration>> GetTimeRegistrationForEmployee(int employeeId);
+    }
+}
