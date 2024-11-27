2dbc86de48d8129b5faf8bbd173fea5704aab168
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2024-11-27 15:18:59 +0100

    Migration to create the TimeRegistration table

--- BethanysPieShopHTM.Core.DomainServices/DatabaseContext/AppDbContext.cs
+++ BethanysPieShopHTM.Core.DomainServices/DatabaseContext/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace BethanysPieShopHTM.Core.DomainServices.DatabaseContext
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Country> Countries { get; set; }
         public DbSet<JobCategory> JobCategories { get; set; }
-        //public DbSet<TimeRegistration> TimeRegistrations { get; set; }
+        public DbSet<TimeRegistration> TimeRegistrations { get; set; }
         
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
---
+++ BethanysPieShopHTM.Core.DomainServices/Migrations/20241127141722_TimeRegistrationTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BethanysPieShopHTM.Core.DomainServices.Migrations
+{
+    /// <inheritdoc />
+    public partial class TimeRegistrationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TimeRegistrations",
+                columns: table => new
+                {
+                    TimeRegistrationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PerformedTaskDescription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeRegistrations", x => x.TimeRegistrationId);
+                    table.ForeignKey(
+                        name: "FK_TimeRegistrations_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeRegistrations_EmployeeId",
+                table: "TimeRegistrations",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeRegistrations");
+        }
+    }
+}
