b6a20cf21e763fafc0f0892555ea432b2f9d8db1
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2024-12-06 11:46:12 +0100

    Adding validation

--- BethanysPieShopHRM.Application/Dtos/EmployeeDto.cs
+++ BethanysPieShopHRM.Application/Dtos/EmployeeDto.cs
@@ -1,16 +1,24 @@
 ﻿using BethanysPieShopHRM.Shared.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace BethanysPieShopHRM.Application.Dtos
 {
     public class EmployeeDto
     {
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50, ErrorMessage = "First name is too long.")]
         public string FirstName { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name is too long.")]
         public string LastName { get; set; } = string.Empty;
 
         public DateTime? BirthDate { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
         public string? Street { get; set; }
@@ -34,6 +42,7 @@ namespace BethanysPieShopHRM.Application.Dtos
         public bool IsOnHoliday { get; set; } = false;
 
 
+        [StringLength(1000, ErrorMessage = "Comment length can't exceed 1000 characters.")]
         public string Comment { get; set; } = string.Empty;
 
         public DateTime? JoinedDate { get; set; }
